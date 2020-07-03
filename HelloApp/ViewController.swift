a51896f889a3e06b846f342954d16c86b8dfdf22
jerrycastillo <gweb1986@gmail.com>
2020-07-03 15:51:56 -0500

    Initial Commit

---
+++ HelloApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by Rex Karnufex on 03/07/20.
+//  Copyright © 2020 GWEB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
